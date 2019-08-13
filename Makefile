3444d47744e9cfdb901518148877b6bcdbb5cbe4
Ahmad Hasya <hasyazidane2@gmail.com>
2019-08-13 08:57:42 +0700

    Parsing Command

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+compile_createxls:
+	g++ createxls.cpp -o createxls.out
+run_createxls:
+	./createxls.out
 compile_hello:
 	g++ helloworld.cpp -o helloworld.out
 run_hello:
---
+++ command.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class command
+{
+public:
+    void list()
+    {
+        cout << "New File (N)" << endl;
+        cout << "Load File (O)" << endl;
+        cout << "About (A)" << endl
+             << endl;
+    }
+    string execute(string option)
+    {
+        if (option == "New File" || option == "N")
+        {
+            return "Success";
+        }
+        else if (option == "Load File" || option == "O")
+        {
+            return "Success";
+        }
+        else if (option == "About" || option == "A")
+        {
+            return "Success";
+        }
+        return "Command Not Found";
+    }
+};
